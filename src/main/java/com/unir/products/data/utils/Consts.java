b89a0e42d7f614d831632312344aabae2f71669f
Sergi Lopez <sergi.lopez94@gmail.com>
2025-06-07 19:45:44 +0200

    Implementation of service and data layer for book catalogue

--- src/main/java/com/unir/products/data/utils/Consts.java
+++ src/main/java/com/unir/products/data/utils/Consts.java
@@ -3,9 +3,17 @@ package com.unir.products.data.utils;
 public class Consts {
 
     //Constants for db entity fields
-    public static final String NAME = "name";
-    public static final String COUNTRY = "country";
-    public static final String DESCRIPTION = "description";
+    public static final String TITULO = "titulo";
+    public static final String AUTOR = "autor";
+    public static final String FECHA_DE_PUBLICACION = "fecha_de_publicacion";
+    public static final String EDITORIAL = "editorial";
+    public static final String CATEGORIA = "categoria";
+    public static final String ISBN = "ISBN";
+    public static final String PORTADA = "portada";
+    public static final String SINOPSIS = "sinopsis";
+    public static final String VALORACION = "valoracion";
     public static final String VISIBLE = "visible";
-
+    public static final String STOCK = "stock";
+    public static final String PRECIO = "precio";
+      
 }
---
+++ src/main/java/com/unir/products/service/BooksService.java
@@ -0,0 +1,23 @@
+package com.unir.products.service;
+
+import java.util.List;
+
+import com.unir.products.data.model.Book;
+import com.unir.products.controller.model.BookDto;
+import com.unir.products.controller.model.CreateBookRequest;
+
+public interface BooksService {
+	
+	List<Book> getBooks(String titulo, String autor, String fechaDePublicacion, String editorial, String categoria, Long isbn, String sinopsis, Double valoracion, Boolean visible, Boolean stock, Double precio);
+	
+	Book getBook(String bookId);
+	
+	Boolean removeBook(String bookId);
+	
+	Book createBook(CreateBookRequest request);
+
+	Book updateBook(String bookId, String updateRequest);
+
+	Book updateBook(String bookId, BookDto updateRequest);
+
+}
